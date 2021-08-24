5e17025c4624aca20a5a78cff276e7f32529505f
devHikary <wrkkarina@gmail.com>
2021-08-24 07:29:14 -0300

    Aula 2.2 TextField

--- func-compo/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ func-compo/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 function FormularioCadastro() {
   return (
     <form>
-      <label>Nome</label>
-      <input type="text"/>
+      <TextField id="nome" label="Nome" variant="outlined" fullWidth="true"/>
 
-      <label>Sobrenome</label>
-      <input type="text"/>
+      <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth="true"/>
 
-      <label>CPF</label>
-      <input type="text"/>
+      <TextField id="cpf" label="CPF" variant="outlined" fullWidth="true"/>
 
       <label>Promoções</label>
       <input type="checkbox"/>
