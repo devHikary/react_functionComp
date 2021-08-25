59ba5f5361f574b4517ee82303377b8ab1e3b5b6
devHikary <wrkkarina@gmail.com>
2021-08-25 06:51:03 -0300

    Aula 3.2 - onChange onSubmit

--- func-compo/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ func-compo/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,46 +1,58 @@
-import React from 'react';
-import { Button, FormControlLabel, Switch, TextField } from '@material-ui/core';
+import React from "react";
+import { Button, FormControlLabel, Switch, TextField } from "@material-ui/core";
 
 function FormularioCadastro() {
+  let nome = "";
+
   return (
-    <form>
-      <TextField margin="normal"
-        id="nome" label="Nome" variant="outlined" fullWidth="true" />
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(nome);
+      }}
+    >
+      <TextField
+        onChange={(evento) => {
+          nome = evento.target.value;
+        }}
+        margin="normal"
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth="true"
+      />
 
-      <TextField margin="normal"
-        id="sobrenome" label="Sobrenome" variant="outlined" fullWidth="true" />
+      <TextField
+        margin="normal"
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth="true"
+      />
 
-      <TextField margin="normal"
-        id="cpf" label="CPF" variant="outlined" fullWidth="true" />
+      <TextField
+        margin="normal"
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth="true"
+      />
 
       <FormControlLabel
-        control={
-          <Switch
-            defaultChecked
-            color="primary"
-            name="promocoes"
-          />
-        }
+        control={<Switch defaultChecked color="primary" name="promocoes" />}
         label="Promoções"
       />
 
       <FormControlLabel
-        control={
-          <Switch
-            defaultChecked
-            color="primary"
-            name="novidades"
-          />
-        }
+        control={<Switch defaultChecked color="primary" name="novidades" />}
         label="Novidades"
       />
 
-      <Button
-        variant="contained" color="primary"
-        type="submit">Cadastrar</Button>
+      <Button variant="contained" color="primary" type="submit">
+        Cadastrar
+      </Button>
     </form>
   );
-
 }
 
 export default FormularioCadastro;
