c75bef89ad579e089207970d934bbfcaebf5661e
devHikary <wrkkarina@gmail.com>
2021-09-05 11:42:28 -0300

    2Aula 2.2 - Renderização condicional

--- func-compo/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ func-compo/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import DadosPessoais from './DadosPessoais';
-import DadosUsuario from './DadosUsuario';
-import DadosEntrega from './DadosEntrega';
+import { Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import DadosEntrega from "./DadosEntrega";
+import DadosPessoais from "./DadosPessoais";
+import DadosUsuario from "./DadosUsuario";
 
 function FormularioCadastro({ aoEnviar, validarCPF }) {
 
+  const [etapaAtual, setEtapaAtual] = useState(1);
+  
+  function formularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosUsuario />;
+      case 1:
+        return <DadosPessoais  aoEnviar={aoEnviar} validarCPF={validarCPF}/>;
+      case 2:
+        return <DadosEntrega/>;
+      default:
+        return <Typography>Erro ao selecionar formulário</Typography>;
+    }
+  }
 
   return (
     <>
-      <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF}/>
-      <DadosUsuario/>
-      <DadosEntrega/>
+      {formularioAtual(etapaAtual)}
     </>
   );
-
 }
+
 
 export default FormularioCadastro;
