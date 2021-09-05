568dbcbd04caad52f7c7bd86f23c703a9479582e
devHikary <wrkkarina@gmail.com>
2021-09-05 12:00:12 -0300

    2Aula 2.4 Injeção de dependências

--- func-compo/src/components/FormularioCadastro/DadosUsuario.jsx
+++ func-compo/src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,20 +1,35 @@
-import { Button, TextField } from '@material-ui/core';
-import React from 'react';
+import { Button, TextField } from "@material-ui/core";
+import React from "react";
 
-function DadosUsuario() {
-
+function DadosUsuario({ aoEnviar }) {
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar();
+      }}
+    >
       <TextField
-        margin="normal" variant="outlined" fullWidth        
-        id="email" label="email" type="email" />
+        margin="normal"
+        variant="outlined"
+        fullWidth
+        id="email"
+        required
+        label="email"
+        type="email"
+      />
       <TextField
-        margin="normal" variant="outlined" fullWidth
-        id="senha" label="senha" type="passaword" />
-      <Button
-        variant="contained" color="primary"
-        type="submit">Cadastrar</Button>
-
+        margin="normal"
+        variant="outlined"
+        fullWidth
+        required
+        id="senha"
+        label="senha"
+        type="passaword"
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Cadastrar
+      </Button>
     </form>
   );
 }
