04727f61db86c0c8db1c1aafcb8946a40a7d45b5
devHikary <wrkkarina@gmail.com>
2021-09-05 13:45:28 -0300

    2Aula 4.5 useContext

--- func-compo/src/App.js
+++ func-compo/src/App.js
@@ -1,23 +1,27 @@
-import { Container, Typography } from '@material-ui/core';
-import React, { Component } from 'react';
-import './App.css';
-import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from "@material-ui/core";
+import React, { Component } from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import ValidacoesCadastro from "./contexts/ValidacoesCadastro";
 // import 'fontsource-roboto';
-import { validarCPF, validarSenha } from './models/cadastro'
+import { validarCPF, validarSenha } from "./models/cadastro";
 
 class App extends Component {
   render() {
     return (
-      <Container
-        component="article"
-        maxWidth="sm"
-      >
-        <Typography
-          variant="h3" align="center" component="h1"
-        >Formulário</Typography >
-        <FormularioCadastro aoEnviar={aoEnviarForm} validacoes={{
-          cpf: validarCPF, senha: validarSenha, nome: validarSenha
-        }} />
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" align="center" component="h1">
+          Formulário
+        </Typography>
+        <ValidacoesCadastro.Provider
+          value={{
+            cpf: validarCPF,
+            senha: validarSenha,
+            nome: validarSenha,
+          }}
+        >
+          <FormularioCadastro aoEnviar={aoEnviarForm} />
+        </ValidacoesCadastro.Provider>
       </Container>
     );
   }
@@ -26,7 +30,5 @@ class App extends Component {
 function aoEnviarForm(dados) {
   console.log(dados);
 }
-
-
 
 export default App;
--- func-compo/src/components/FormularioCadastro/DadosPessoais.jsx
+++ func-compo/src/components/FormularioCadastro/DadosPessoais.jsx
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { useState, useContext } from "react";
 import { Button, FormControlLabel, Switch, TextField } from "@material-ui/core";
+import ValidacoesCadastro from "../../contexts/ValidacoesCadastro";
 
-function DadosPessoais({ aoEnviar, validacoes }) {
+function DadosPessoais({ aoEnviar }) {
   const [nome, setNome] = useState("");
   const [sobrenome, setSobrenome] = useState("");
   const [cpf, setCpf] = useState("");
@@ -11,6 +12,8 @@ function DadosPessoais({ aoEnviar, validacoes }) {
     cpf: { valido: true, texto: "" },
     nome: { valido: true, texto: "" },
   });
+
+  const validacoes = useContext(ValidacoesCadastro);
 
   function validarCampos(event) {
     const { name, value } = event.target;
